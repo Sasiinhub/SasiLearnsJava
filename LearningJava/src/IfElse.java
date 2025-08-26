16eff8d41ef5063d432bc44cc1e8a507a486c920
Sasiinhub <sasiinweb@gmail.com>
2025-08-26 18:30:19 +0530

    Create IfElse.java

---
+++ LearningJava/src/IfElse.java
@@ -0,0 +1,16 @@
+class IfElse {
+    public static void main(String args) {
+          int month = 4;  //April
+          if(month == 12 || month == 1 || month == 2)
+             season = 'Winter";
+          else if(month == 3 || month == 4 || month == 5)
+            season = "Spring";
+          else if(month == 6 || month == 7 || month == 8)
+            season = "Summer";
+          else if (month == 9 || month == 10 || month == 11)
+            season = "Autumn";
+          else 
+            season = "Bougus Month";
+      system.out.println("April is in the " +  season + ".");
+    }
+}
