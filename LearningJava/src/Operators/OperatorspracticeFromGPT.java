f0b22de2bbd27010d349a4121227bd9a9d811e57
Sasiinhub <sasiinweb@gmail.com>
2025-08-29 16:17:12 +0530

    Create OperatorspracticeFromGPT.java
    from chat gpt

---
+++ LearningJava/src/Operators/OperatorspracticeFromGPT.java
@@ -0,0 +1,27 @@
+
+ public class OperatorspracticeFromGPT {
+    public static void main(String args[]) {
+        
+//Precedence: * before +
+        System.out.println(5 + 2 * 3); // Parentheses override
+        System.out.println(10 - 6 / 3);
+        System.out.println(10-6 / 2 * 3);
+        int c = 15, b = 4;
+        System.out.println(c % b);
+        int a = 5;
+        a += 3 * 2;
+System.out.println(a);
+
+int result = (10 - 6 / 2 * 3);
+System.out.println(result);
+
+
+int x = 5;
+System.out.println(x++ + ++x);
+
+
+
+
+    }
+} 
+
