33acfe796cdc732aa769523b74ac96cbff9324c6
Sasiinhub <sasiinweb@gmail.com>
2025-09-01 11:39:18 +0530

    Create ForTick.java

---
+++ LearningJava/src/ControlStatements/IterationStatements/ForTick.java
@@ -0,0 +1,9 @@
+class ForTick {
+  public static void main(String args[]) {
+    int n;
+
+    for(n=10; n>0; n--)
+      System.out.println("tick " + n);
+    }
+}
+\* often the variable that controls a for loop is needed only for the purpose of the loop and is not used elsewhere. When this is
