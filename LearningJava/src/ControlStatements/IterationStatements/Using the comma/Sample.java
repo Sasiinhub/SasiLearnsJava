c5c0bee108f4ccc2ad5e037844754a70dc4da6d5
Sasiinhub <sasiinweb@gmail.com>
2025-09-02 15:25:25 +0530

    Create Sample.java

---
+++ LearningJava/src/ControlStatements/IterationStatements/Using the comma/Sample.java
@@ -0,0 +1,15 @@
+/*  Using the Comma
+  There will be times when we will want to include more than one statement in the initialization and iteration portions
+  of the for loop.
+*/
+  class sample {
+    public static void main(String args[]) {
+      int a, b;
+
+      for(a=1; a<b; a++) {
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        b--;
+      }
+    }
+}
