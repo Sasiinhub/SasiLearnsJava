f7082df98d9079b3ee5e220b6d7973d877587c63
Sasiinhub <sasiinweb@gmail.com>
2025-09-02 14:57:03 +0530

    Update ForTick.java
    Two methods of initialization used.

--- LearningJava/src/ControlStatements/IterationStatements/ForTick.java
+++ LearningJava/src/ControlStatements/IterationStatements/ForTick.java
@@ -6,4 +6,15 @@ class ForTick {
       System.out.println("tick " + n);
     }
 }
-\* often the variable that controls a for loop is needed only for the purpose of the loop and is not used elsewhere. When this is
+\* often the variable that controls a for loop is needed only for the purpose of the loop and is not used elsewhere. When this is the case,
+  it is possible to declare the variable inside the initialization portion of the for.
+  \*
+  *//Declaring a loop control variable inside the for.
+  *class ForTick {
+  *  public static void main(String args[]) {
+  * 
+  *//here, n is declared inside of the for loop
+  * for(int n=10; n>0; n--)
+    System.out.println("tick " + n);
+  }
+}
