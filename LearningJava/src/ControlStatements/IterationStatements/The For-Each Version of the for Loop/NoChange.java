b645d4c35567e8231754b336a6f8991136c8d7ea
Sasiinhub <sasiinweb@gmail.com>
2025-09-18 11:37:11 +0530

    Update NoChange.java
    just pasting the contents on extended description into the code area.

--- LearningJava/src/ControlStatements/IterationStatements/The For-Each Version of the for Loop/NoChange.java
+++ LearningJava/src/ControlStatements/IterationStatements/The For-Each Version of the for Loop/NoChange.java
@@ -14,4 +14,11 @@ class NoChange {
 
   System.out.println();
     }
-}
+} 
+/* the one of the important point to understand about the for-each style loop.
+    its iteration variable is " read only" as it relates to the underlying array. 
+    An assignment  to the iteration statement has no effect on the underlying array. 
+    in other words we can't change the contents of the array by assigning the iteration variable a new value. 
+    the first for loop increases the value of the iteration variable by a factor of 10. However this assignment has no effect on underlying 
+    array nums, as the second for loop illustrates.
+    the output proves this point. */
