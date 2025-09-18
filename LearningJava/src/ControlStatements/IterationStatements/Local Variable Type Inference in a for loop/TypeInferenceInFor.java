21867fdaffdcd131f182c42fbaaf8b36089f61bd
Sasiinhub <sasiinweb@gmail.com>
2025-09-18 17:31:49 +0530

    Create TypeInferenceInFor.java

---
+++ LearningJava/src/ControlStatements/IterationStatements/Local Variable Type Inference in a for loop/TypeInferenceInFor.java
@@ -0,0 +1,19 @@
+// use type inference in a for loop.
+class TypeInferenceInFor {
+  public static void main(String args[]) {
+    // use type inference with the loop control variable.
+  System.ouot.print("Values of x: ");
+    for(var x = 2.5; x< 100.0; x = x*2)
+      System.oout.print(x + " ");
+
+  System.out.println();
+
+  //Use type inference with the iteration variable.
+  int[] nums = { 1, 2, 3, 4, 5, 6};
+    System.out.print("Values in nums array: ";
+    for (var v : nums)
+      System.out.print(v + " ");
+
+  System.out.println();
+  }
+}
